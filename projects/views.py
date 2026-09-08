2e9b198f410193720a5083b0dac15a850189af3f
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-09-08 19:12:27 -0300

    feat: añadir controladores

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Project, Deliverable, Comment
+from .serializers import ProjectSerializer, DeliverableSerializer, CommentSerializer
 
-# Create your views here.
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all().order_by('-created_at')
+    serializer_class = ProjectSerializer
+    
+    # Configuración de filtros y búsquedas
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['client', 'status', 'priority']
+    search_fields = ['name', 'deliverables__title']
+
+class DeliverableViewSet(viewsets.ModelViewSet):
+    queryset = Deliverable.objects.all()
+    serializer_class = DeliverableSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
